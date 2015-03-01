d81f7921ca3fc45c9d0d7ba047e12147c2f70559
Mike Thomas <mike_thomas1@me.com>
2015-03-01 00:23:47 -0600

    Created Xcode project

---
+++ Seeing iBot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Seeing iBot
+//
+//  Created by Michael Thomas on 3/1/15.
+//  Copyright (c) 2015 Mike Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
