e644b606c75c6985143f1096915f05acc1d1e077
Michael Thomas <mike_thomas1@me.com>
2016-02-11 23:12:31 -0600

    Fixed layout for iPhone and iPad

--- Seeing iBot/ViewController.swift
+++ Seeing iBot/ViewController.swift
@@ -12,60 +12,34 @@ import SpriteKit
 class ViewController: UIViewController, UIWebViewDelegate {
     @IBOutlet private var videoStreamView: UIWebView!
     @IBOutlet private var refreshStream: UIButton!
-    var ipAddress: String!
+    @IBOutlet weak var joyStickView: SKView!
+    
+    // TODO: disable app transport security
+    let ipAddress = "192.168.1.1"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         videoStreamView.delegate = self
-        
-        let size = CGSizeMake(736, 414)
-        
+        let size = joyStickView.frame.size
         let scene = GameScene(size: size) as GameScene
+        
         // Configure the view.
-        let skView = self.view as! SKView
-        skView.showsFPS = true
-        skView.showsNodeCount = true
+        joyStickView.showsFPS = true
+        joyStickView.showsNodeCount = true
         
         /* Sprite Kit applies additional optimizations to improve rendering performance */
-        skView.ignoresSiblingOrder = true
+        joyStickView.ignoresSiblingOrder = true
+
+        scene.scaleMode = .Fill
         
-        /* Set the scale mode to scale to fit the window */
-        scene.scaleMode = .AspectFill
-        
-        skView.presentScene(scene)
+        joyStickView.presentScene(scene)
     }
     
-    override func viewDidAppear(animated: Bool)
-    {
+    override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(true)
 
-        let defaults = NSUserDefaults.standardUserDefaults()
-        if let address = defaults.stringForKey("ipAddress") {
-            ipAddress = address
-            loadStream(address)
-        } else {
-            showAlert()
-        }
-    }
-    
-    private func showAlert() {
-        let alertController = UIAlertController(title: "IP", message: "Please enter the IP address of the Raspberry Pi \n i.e. 192.168.2.69", preferredStyle: .Alert)
-        
-        alertController.addTextFieldWithConfigurationHandler { (textField) in
-            textField.keyboardType = UIKeyboardType.NumbersAndPunctuation
-            textField.placeholder = "IP Address"
-        }
-        
-        let ipAction = UIAlertAction(title: "Set IP", style: .Default) { (_) in
-            let ipTextField = alertController.textFields![0] as UITextField
-            self.ipAddress = ipTextField.text
-            self.loadStream(self.ipAddress)
-        }
-        
-        alertController.addAction(ipAction)
-        
-        presentViewController(alertController, animated: true, completion: nil)
+        loadStream(ipAddress)
     }
     
     private func loadStream(ipAddress: String) {
@@ -77,31 +51,21 @@ class ViewController: UIViewController, UIWebViewDelegate {
     }
     
     func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
-        ipAddress = nil
-        let alertController = UIAlertController(title: "Error Loading", message: "Encountered an error: \(error?.localizedDescription) \n Make sure that the Raspberry Pi is turned on and you've entered the correct IP address. i.e. 192.168.2.69", preferredStyle: .Alert)
+        // TODO: add the command in the error message
+//        let alertController = UIAlertController(title: "Error Loading", message: "Encountered an error: \(error?.localizedDescription) \n Make sure that the Raspberry Pi is turned on and have adhoc wifi running", preferredStyle: .Alert)
+//        
+//        let retry = UIAlertAction(title: "Retry", style: .Default) { (_) in
+//            self.loadStream(self.ipAddress)
+//        }
+//        
+//        alertController.addAction(retry)
         
-        alertController.addTextFieldWithConfigurationHandler { (textField) in
-            textField.keyboardType = UIKeyboardType.NumbersAndPunctuation
-            textField.placeholder = "IP Address"
-        }
-        
-        let ipAction = UIAlertAction(title: "Set IP", style: .Default) { (_) in
-            let ipTextField = alertController.textFields![0] as UITextField
-            self.ipAddress = ipTextField.text
-            self.loadStream(self.ipAddress)
-        }
-        
-        alertController.addAction(ipAction)
-        
-        presentViewController(alertController, animated: true, completion: nil)
+//        presentViewController(alertController, animated: true, completion: nil)
     }
     
     func webViewDidFinishLoad(webView: UIWebView) {
         // TODO: hide loading spinner
         refreshStream.enabled = true
-        
-        let defaults = NSUserDefaults.standardUserDefaults()
-        defaults.setObject(self.ipAddress, forKey: "ipAddress")
     }
     
     func webViewDidStartLoad(webView: UIWebView) {
@@ -110,16 +74,9 @@ class ViewController: UIViewController, UIWebViewDelegate {
     }
     
     @IBAction func refreshButtonPressed(sender: AnyObject) {
-        // TODO: move logic into function 
-        // TODO: show loading spinner
-        let defaults = NSUserDefaults.standardUserDefaults()
-        if let address = defaults.stringForKey("ipAddress") {
-            ipAddress = address
-            loadStream(address)
-        } else {
-            showAlert()
-        }
+        // TODO: once we have adhoc wifi and static ip address use the static 
+        // ip here
+        loadStream(ipAddress)
     }
-
 }
 
